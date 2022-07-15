767270b9eede09c84b3114742560093e394f3276
Katarzyna Juchniewicz <juchkasia@gmail.com>
2022-07-15 14:48:50 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0; i < expr.length; i+=10) {
+        let  symbol = expr.slice(i, i + 10)
+        result += symbol == '**********' ? ' ' : MORSE_TABLE[getMorseSymbol(symbol)];
+    }
+    return result;
+}
+
+let getMorseSymbol = function(symbol) {
+    let result = '';
+    for(let i=0; i< symbol.length; i += 2) {
+        switch(symbol.slice(i, i + 2)) {
+            case '00':
+                result += '';
+                break;
+            case '10':
+                result += '.';
+                break;
+            case '11':
+                result += '-';
+                break;
+        }
+    }
+    return result;
 }
 
 module.exports = {
